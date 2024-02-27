56d3c72d9d7613b64a683205fd1ab50c543c2d62
phung thai son <sondzvodoi@gmail.con>
2024-02-27 22:29:12 +0700

    connect db, mugrations

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,9 +1,14 @@
 import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
+import { UserModule } from './modules/user/user.module';
+import { TypeOrmModule } from '@nestjs/typeorm';
+import { dataSourceOptions } from '../db/data-source';
 
 @Module({
-  imports: [],
+  imports: [
+    UserModule,
+    TypeOrmModule.forRoot(dataSourceOptions),],
   controllers: [AppController],
   providers: [AppService],
 })
